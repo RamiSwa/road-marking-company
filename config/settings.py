bea353ed074460d0a8fd44aee852dac13fee3888
RamiSwa <118728140+RamiSwa@users.noreply.github.com>
2025-03-18 06:08:48 +0200

    CSRF2

--- config/settings.py
+++ config/settings.py
@@ -34,7 +34,6 @@ ALLOWED_HOSTS=[
     '127.0.0.1',
     ]
 
-CSRF_TRUSTED_ORIGINS = [
 CSRF_TRUSTED_ORIGINS = os.getenv(
     'CSRF_TRUSTED_ORIGINS', 
     default='"https://road-marking-company-production.up.railway.app",http://127.0.0.1:8000'
